bd5d39ce46c8c52712a4910ac9b9076ffce28974
Jagadeesan R V <93546094+JagadeesanRajalakshmiVellaichamy@users.noreply.github.com>
2023-09-05 03:46:41 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -29,9 +29,9 @@ st.set_page_config(layout="wide")
 st.title('Indian General Election 2019 Youtube Sentiment Dashboard')
 
 #Step3: Read the file from
-comments = 'https://raw.githubusercontent.com/JagadeesanRajalakshmiVellaichamy/Dissertation2023/main/Youtube_Clean_dataframe.csv'
+comments = 'https://raw.githubusercontent.com/JagadeesanRajalakshmiVellaichamy/Dissertation2023/main/data/Youtube_Clean_dataframe.csv'
 df = pd.read_csv(comments)
-st.write(df.head())
+
 #Step4: Plotting the graphs for the dashboard (Analysis period from Jan to Apr 2019 is considered)
 #########################################----SECTION-1----#################################################
 #CHART-1.1: ANALYSIS PERIOD - OVERALL PERCENTAGE OF COMMENTS ABOUT BJP AND CONGRESS
@@ -507,7 +507,7 @@ with right_column3:
 
 #########################################----SECTION-7----#################################################
 st.markdown("SECTION-6: mBert BASE VS FINE TUNED MODEL COMPARISON BY REGIONAL LANGUAGES")
-NLPmetrics = pd.read_csv('https://rawgit.com/JagadeesanRajalakshmiVellaichamy/Dissertation2023/main/NLP_mBERT_Metrics.csv')
+NLPmetrics = pd.read_csv('https://rawgit.com/JagadeesanRajalakshmiVellaichamy/Dissertation2023/main/data/NLP_mBERT_Metrics.csv')
 
 #CHART7.1: Displaying the Trained model metrics - BAR plots
 #filter1
