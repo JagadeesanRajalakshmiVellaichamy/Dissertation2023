4dc733b31b9662cdc21cd0c853f30589c062ea5e
Jagadeesan R V <93546094+JagadeesanRajalakshmiVellaichamy@users.noreply.github.com>
2023-09-05 03:36:44 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -29,8 +29,8 @@ st.set_page_config(layout="wide")
 st.title('Indian General Election 2019 Youtube Sentiment Dashboard')
 
 #Step3: Read the file from
-comments = requests.get('https://rawgit.com/JagadeesanRajalakshmiVellaichamy/Dissertation2023/main/Youtube_Clean_dataframe.csv').content
-df = pd.read_csv(io.StringIO(comments.decode('utf-8')))
+comments = 'https://raw.githubusercontent.com/JagadeesanRajalakshmiVellaichamy/Dissertation2023/main/Youtube_Clean_dataframe.csv'
+df = pd.read_csv(comments)
 st.write(df.head())
 #Step4: Plotting the graphs for the dashboard (Analysis period from Jan to Apr 2019 is considered)
 #########################################----SECTION-1----#################################################
